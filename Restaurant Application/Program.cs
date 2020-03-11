00874e2b9b68aa0e3ce04b6f74865d9c62596731
Lucavdberg <45539858+Lucavdberg@users.noreply.github.com>
2020-03-11 12:39:46 +0100

    Last test
    last test with github, before starting the project!

--- Restaurant Application/Program.cs
+++ Restaurant Application/Program.cs
@@ -7,6 +7,7 @@ namespace Restaurant_Application
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("De laatste test voordat het allemaal werkt!");
         }
     }
 }
