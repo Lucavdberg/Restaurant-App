7c2d35b6ea35989ddbae62032496fe781ae4620b
Fatih <60482313+Fatih-github@users.noreply.github.com>
2020-04-20 17:03:09 +0200

    bulk update 2
    De email class stuurt nu een mail naar de gebruiker wanneer er te vaak verkeerd wordt ingelogd. Ook helpt het de gebruiker om zijn email adres te wijzigen als dit tijdens het maken van zijn account fout was ingevuld of als de email adres ongeldig is.
    De main bevat nu nog minder code.
    Alle tuples voor het menu zijn vervangen door jagged arrays.
    JsonClassgerechten is nu gevuld met jagged arrays voor het opslaan van het menu voor elke dag.
    JsonClassGerechtenIngevuld vult nu de json file genaamd JsonClassgerechten met het menu van elke dag. Dit word gedaan met jagged arrays.
    De gerechten class is nu aangepast waardoor het nu gebruik maakt van de json file voor het menu genaamd jsonClassGerechtenIngevuld. Ook is de code aangepast zodat er gebruik kan worden gemaakt van de Json.
    De Menu class is aangepast waardoor het nu gebruik maakt van de json file voor het menu genaamd jsonClassGerechtenIngevuld. In de menu class word er nu ook gebruik gemaakt van nested loops om door gerechten te zoeken.
    Het programma loopt niet meer dood en stuurt de gebruiker weer terug naar het hoofdscherm.
    Een nieuwe Class aangemaakt genaamd Tafel. Aan deze Class is er ook een json file aan verbonden. Deze Class wordt gebruikt in de reservering Class en heeft als doel om het aantal personen van een reservering af te trekken van het totaal aantal mensen dat aanwezig kan zijn in het restaurant op die dag (het restaurant heeft een maximum van 50 personen). Dit werkt nog niet helemaal.
    Een nieuwe Json Class aangemaakt genaamd JsonClassTafels. Hierin wordt het aantal tafels ingevuld.
    De customerlogin Class is veranderd. Nu kunnen er meerdere gebruikers een account aanmaken en met deze gegevens inloggen. Ook kan een gebruiker bestaande reservaties bekijken.
    De JsonClassLogin is veranderd waarbij er lists van inloggegevens worden opgeslagen van gebruikers.

--- Restaurant Application/Menu.cs
+++ Restaurant Application/Menu.cs
@@ -3,175 +3,81 @@
 public class Menu
 {
 
-    public string[] MenuVanDeDagFunc(string day,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> maandag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> maandag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> dinsdag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> dinsdag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> woensdag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> woensdag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> donderdag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> donderdag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> vrijdag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> vrijdag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> zaterdag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> zaterdag_twee,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> zondag_een,
-            Tuple<Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>, Tuple<string, string, string, string>> zondag_twee)
+    public void MenuVanDeDagFunc(string day, JsonClassGerechten gerechtClass)
     {
-
         if (day == "maandag" || day == "Maandag" || day == "menu maandag" || day == "het menu van maandag")
         {
-            string[] a = new string[] {
-              maandag_een.Item1.Item1, maandag_een.Item1.Item2, maandag_een.Item1.Item3, maandag_een.Item1.Item4, "",
-              maandag_een.Item2.Item1, maandag_een.Item2.Item2, maandag_een.Item2.Item3, "",
-              maandag_een.Item3.Item1, maandag_een.Item3.Item2, maandag_een.Item3.Item3, maandag_een.Item3.Item4, "",
-              maandag_een.Item4.Item1, maandag_een.Item4.Item2, maandag_een.Item4.Item3, "",
-              maandag_een.Item5.Item1, maandag_een.Item5.Item2, maandag_een.Item5.Item3, maandag_een.Item5.Item4, "",
-              maandag_een.Item6.Item1, maandag_een.Item6.Item2, maandag_een.Item6.Item3, maandag_een.Item6.Item4, "",
-              maandag_een.Item7.Item1, maandag_een.Item7.Item2, maandag_een.Item7.Item3, "",
-              maandag_twee.Item1.Item1, maandag_twee.Item1.Item2, maandag_twee.Item1.Item3, maandag_twee.Item1.Item4, "",
-              maandag_twee.Item2.Item1, maandag_twee.Item2.Item2, maandag_twee.Item2.Item3, maandag_twee.Item2.Item4, "",
-              maandag_twee.Item3.Item1, maandag_twee.Item3.Item2, maandag_twee.Item3.Item3, maandag_twee.Item3.Item4, "",
-              maandag_twee.Item4.Item1, maandag_twee.Item4.Item2, maandag_twee.Item4.Item3, maandag_twee.Item4.Item4, ""
-              };
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < gerechtClass.maandag.Length; i++)
             {
-                Console.WriteLine(a[i]);
+                for (int j = 0; j < gerechtClass.maandag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.maandag[i][j]);
+                }
             }
-            return a;
         }
         else if (day == "dinsdag" || day == "Dinsdag" || day == "menu dinsdag" || day == "het menu van dinsdag")
         {
-            string[] b = new string[] {
-              dinsdag_een.Item1.Item1, dinsdag_een.Item1.Item2, dinsdag_een.Item1.Item3, dinsdag_een.Item1.Item4, "",
-              dinsdag_een.Item2.Item1, dinsdag_een.Item2.Item2, dinsdag_een.Item2.Item3, dinsdag_een.Item2.Item4, "",
-              dinsdag_een.Item3.Item1, dinsdag_een.Item3.Item2, dinsdag_een.Item3.Item3, dinsdag_een.Item3.Item4, "",
-              dinsdag_een.Item4.Item1, dinsdag_een.Item4.Item2, dinsdag_een.Item4.Item3, "",
-              dinsdag_een.Item5.Item1, dinsdag_een.Item5.Item2, dinsdag_een.Item5.Item3, dinsdag_een.Item5.Item4, "",
-              dinsdag_een.Item6.Item1, dinsdag_een.Item6.Item2, dinsdag_een.Item6.Item3, dinsdag_een.Item6.Item4, "",
-              dinsdag_een.Item7.Item1, dinsdag_een.Item7.Item2, dinsdag_een.Item7.Item3, dinsdag_een.Item7.Item4, "",
-              dinsdag_twee.Item1.Item1, dinsdag_twee.Item1.Item2, dinsdag_twee.Item1.Item3, dinsdag_twee.Item1.Item4, "",
-              dinsdag_twee.Item2.Item1, dinsdag_twee.Item2.Item2, dinsdag_twee.Item2.Item3, "",
-              dinsdag_twee.Item3.Item1, dinsdag_twee.Item3.Item2, dinsdag_twee.Item3.Item3, dinsdag_twee.Item3.Item4, "",
-              dinsdag_twee.Item4.Item1, dinsdag_twee.Item4.Item2, dinsdag_twee.Item4.Item3, ""
-              };
-            for (int i = 0; i < b.Length; i++)
+            for (int i = 0; i < gerechtClass.dinsdag.Length; i++)
             {
-                Console.WriteLine(b[i]);
+                for (int j = 0; j < gerechtClass.dinsdag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.dinsdag[i][j]);
+                }
             }
-            return b;
         }
         else if (day == "woensdag" || day == "Woensdag" || day == "menu woensdag" || day == "het menu van woensdag")
         {
-            string[] c = new string[] {
-              woensdag_een.Item1.Item1, woensdag_een.Item1.Item2, woensdag_een.Item1.Item3, woensdag_een.Item1.Item4, "",
-              woensdag_een.Item2.Item1, woensdag_een.Item2.Item2, woensdag_een.Item2.Item3, "",
-              woensdag_een.Item3.Item1, woensdag_een.Item3.Item2, woensdag_een.Item3.Item3, woensdag_een.Item3.Item4, "",
-              woensdag_een.Item4.Item1, woensdag_een.Item4.Item2, woensdag_een.Item4.Item3, woensdag_een.Item4.Item4, "",
-              woensdag_een.Item5.Item1, woensdag_een.Item5.Item2, woensdag_een.Item5.Item3, "",
-              woensdag_een.Item6.Item1, woensdag_een.Item6.Item2, woensdag_een.Item6.Item3, woensdag_een.Item6.Item4, "",
-              woensdag_een.Item7.Item1, woensdag_een.Item7.Item2, woensdag_een.Item7.Item3, woensdag_een.Item7.Item4, "",
-              woensdag_twee.Item1.Item1, woensdag_twee.Item1.Item2, woensdag_twee.Item1.Item3, woensdag_twee.Item1.Item4, "",
-              woensdag_twee.Item2.Item1, woensdag_twee.Item2.Item2, woensdag_twee.Item2.Item3, woensdag_twee.Item2.Item4, "",
-              woensdag_twee.Item3.Item1, woensdag_twee.Item3.Item2, woensdag_twee.Item3.Item3, woensdag_twee.Item3.Item4, "",
-              woensdag_twee.Item4.Item1, woensdag_twee.Item4.Item2, woensdag_twee.Item4.Item3, ""
-              };
-            for (int i = 0; i < c.Length; i++)
+            for (int i = 0; i < gerechtClass.woensdag.Length; i++)
             {
-                Console.WriteLine(c[i]);
+                for (int j = 0; j < gerechtClass.woensdag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.woensdag[i][j]);
+                }
             }
-            return c;
         }
         else if (day == "donderdag" || day == "Donderdag" || day == "menu donderdag" || day == "het menu van donderdag")
         {
-            string[] d = new string[] {
-              donderdag_een.Item1.Item1, donderdag_een.Item1.Item2, donderdag_een.Item1.Item3, "",
-              donderdag_een.Item2.Item1, donderdag_een.Item2.Item2, donderdag_een.Item2.Item3, "",
-              donderdag_een.Item3.Item1, donderdag_een.Item3.Item2, donderdag_een.Item3.Item3, donderdag_een.Item3.Item4, "",
-              donderdag_een.Item4.Item1, donderdag_een.Item4.Item2, donderdag_een.Item4.Item3, donderdag_een.Item4.Item4, "",
-              donderdag_een.Item5.Item1, donderdag_een.Item5.Item2, donderdag_een.Item5.Item3, donderdag_een.Item5.Item4, "",
-              donderdag_een.Item6.Item1, donderdag_een.Item6.Item2, donderdag_een.Item6.Item3, donderdag_een.Item6.Item4, "",
-              donderdag_een.Item7.Item1, donderdag_een.Item7.Item2, donderdag_een.Item7.Item3, donderdag_een.Item7.Item4, "",
-              donderdag_twee.Item1.Item1, donderdag_twee.Item1.Item2, donderdag_twee.Item1.Item3, donderdag_twee.Item1.Item4, "",
-              donderdag_twee.Item2.Item1, donderdag_twee.Item2.Item2, donderdag_twee.Item2.Item3, donderdag_twee.Item2.Item4, "",
-              donderdag_twee.Item3.Item1, donderdag_twee.Item3.Item2, donderdag_twee.Item3.Item3, donderdag_twee.Item3.Item4, "",
-              donderdag_twee.Item4.Item1, donderdag_twee.Item4.Item2, donderdag_twee.Item4.Item3, donderdag_twee.Item4.Item4, ""
-              };
-            for (int i = 0; i < d.Length; i++)
+            for (int i = 0; i < gerechtClass.donderdag.Length; i++)
             {
-                Console.WriteLine(d[i]);
+                for (int j = 0; j < gerechtClass.donderdag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.donderdag[i][j]);
+                }
             }
-            return d;
         }
         else if (day == "vrijdag" || day == "Vrijdag" || day == "menu vrijdag" || day == "het menu van vrijdag")
         {
-            string[] e = new string[] {
-              vrijdag_een.Item1.Item1, vrijdag_een.Item1.Item2, vrijdag_een.Item1.Item3, vrijdag_een.Item1.Item4, "",
-              vrijdag_een.Item2.Item1, vrijdag_een.Item2.Item2, vrijdag_een.Item2.Item3, vrijdag_een.Item2.Item4, "",
-              vrijdag_een.Item3.Item1, vrijdag_een.Item3.Item2, vrijdag_een.Item3.Item3, vrijdag_een.Item3.Item4, "",
-              vrijdag_een.Item4.Item1, vrijdag_een.Item4.Item2, vrijdag_een.Item4.Item3, vrijdag_een.Item4.Item4, "",
-              vrijdag_een.Item5.Item1, vrijdag_een.Item5.Item2, vrijdag_een.Item5.Item3, vrijdag_een.Item5.Item4, "",
-              vrijdag_een.Item6.Item1, vrijdag_een.Item6.Item2, vrijdag_een.Item6.Item3, vrijdag_een.Item6.Item4, "",
-              vrijdag_een.Item7.Item1, vrijdag_een.Item7.Item2, vrijdag_een.Item7.Item3, vrijdag_een.Item7.Item4, "",
-              vrijdag_twee.Item1.Item1, vrijdag_twee.Item1.Item2, vrijdag_twee.Item1.Item3, "",
-              vrijdag_twee.Item2.Item1, vrijdag_twee.Item2.Item2, vrijdag_twee.Item2.Item3, vrijdag_twee.Item2.Item4, "",
-              vrijdag_twee.Item3.Item1, vrijdag_twee.Item3.Item2, vrijdag_twee.Item3.Item3, vrijdag_twee.Item3.Item4, "",
-              vrijdag_twee.Item4.Item1, vrijdag_twee.Item4.Item2, vrijdag_twee.Item4.Item3, vrijdag_twee.Item4.Item4, ""
-              };
-            for (int i = 0; i < e.Length; i++)
+            for (int i = 0; i < gerechtClass.vrijdag.Length; i++)
             {
-                Console.WriteLine(e[i]);
+                for (int j = 0; j < gerechtClass.vrijdag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.vrijdag[i][j]);
+                }
             }
-            return e;
         }
         else if (day == "zaterdag" || day == "Zaterdag" || day == "menu zaterdag" || day == "het menu van zaterdag")
         {
-            string[] f = new string[] {
-              zaterdag_een.Item1.Item1, zaterdag_een.Item1.Item2, zaterdag_een.Item1.Item3, zaterdag_een.Item1.Item4, "",
-              zaterdag_een.Item2.Item1, zaterdag_een.Item2.Item2, zaterdag_een.Item2.Item3, zaterdag_een.Item2.Item4, "",
-              zaterdag_een.Item3.Item1, zaterdag_een.Item3.Item2, zaterdag_een.Item3.Item3, zaterdag_een.Item3.Item4, "",
-              zaterdag_een.Item4.Item1, zaterdag_een.Item4.Item2, zaterdag_een.Item4.Item3, zaterdag_een.Item4.Item4, "",
-              zaterdag_een.Item5.Item1, zaterdag_een.Item5.Item2, zaterdag_een.Item5.Item3, zaterdag_een.Item5.Item4, "",
-              zaterdag_een.Item6.Item1, zaterdag_een.Item6.Item2, zaterdag_een.Item6.Item3, zaterdag_een.Item6.Item4, "",
-              zaterdag_een.Item7.Item1, zaterdag_een.Item7.Item2, zaterdag_een.Item7.Item3, zaterdag_een.Item7.Item4, "",
-              zaterdag_twee.Item1.Item1, zaterdag_twee.Item1.Item2, zaterdag_twee.Item1.Item3, zaterdag_twee.Item1.Item4, "",
-              zaterdag_twee.Item2.Item1, zaterdag_twee.Item2.Item2, zaterdag_twee.Item2.Item3, zaterdag_twee.Item2.Item4, "",
-              zaterdag_twee.Item3.Item1, zaterdag_twee.Item3.Item2, zaterdag_twee.Item3.Item3, "",
-              zaterdag_twee.Item4.Item1, zaterdag_twee.Item4.Item2, zaterdag_twee.Item4.Item3, zaterdag_twee.Item4.Item4, ""
-              };
-            for (int i = 0; i < f.Length; i++)
+            for (int i = 0; i < gerechtClass.zaterdag.Length; i++)
             {
-                Console.WriteLine(f[i]);
+                for (int j = 0; j < gerechtClass.zaterdag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.zaterdag[i][j]);
+                }
             }
-            return f;
         }
         else if (day == "zondag" || day == "Zondag" || day == "menu zondag" || day == "het menu van zondag")
         {
-            string[] g = new string[] {
-              zondag_een.Item1.Item1, zondag_een.Item1.Item2, zondag_een.Item1.Item3, zondag_een.Item1.Item4, "",
-              zondag_een.Item2.Item1, zondag_een.Item2.Item2, zondag_een.Item2.Item3, zondag_een.Item2.Item4, "",
-              zondag_een.Item3.Item1, zondag_een.Item3.Item2, zondag_een.Item3.Item3, zondag_een.Item3.Item4, "",
-              zondag_een.Item4.Item1, zondag_een.Item4.Item2, zondag_een.Item4.Item3, "",
-              zondag_een.Item5.Item1, zondag_een.Item5.Item2, zondag_een.Item5.Item3, zondag_een.Item5.Item4, "",
-              zondag_een.Item6.Item1, zondag_een.Item6.Item2, zondag_een.Item6.Item3, zondag_een.Item6.Item4, "",
-              zondag_een.Item7.Item1, zondag_een.Item7.Item2, zondag_een.Item7.Item3, zondag_een.Item7.Item4, "",
-              zondag_twee.Item1.Item1, zondag_twee.Item1.Item2, zondag_twee.Item1.Item3, zondag_twee.Item1.Item4, "",
-              zondag_twee.Item2.Item1, zondag_twee.Item2.Item2, zondag_twee.Item2.Item3, zondag_twee.Item2.Item4, "",
-              zondag_twee.Item3.Item1, zondag_twee.Item3.Item2, zondag_twee.Item3.Item3, "",
-              zondag_twee.Item4.Item1, zondag_twee.Item4.Item2, zondag_twee.Item4.Item3, ""
-              };
-            for (int i = 0; i < g.Length; i++)
+            for (int i = 0; i < gerechtClass.zondag.Length; i++)
             {
-                Console.WriteLine(g[i]);
+                for (int j = 0; j < gerechtClass.zondag[i].Length; j++)
+                {
+                    Console.WriteLine(gerechtClass.zondag[i][j]);
+                }
             }
-            return g;
         }
-        Console.WriteLine("Type een dag in ");
-        return new string[] { };
+        Console.WriteLine("voer hier alleen dagen in van de week");
+        Console.WriteLine("klik op een toets om terug te keren naar het hoofdmenu");
+        Console.ReadKey();
     }
-
-
-
 }
 
