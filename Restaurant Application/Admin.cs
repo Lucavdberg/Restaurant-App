37c35cee74dcd1d04e551ab01c453cc1eb23faa6
Lucavdberg <45539858+Lucavdberg@users.noreply.github.com>
2020-04-27 18:45:15 +0200

    gedeelte admin rechten

---
+++ Restaurant Application/Admin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+public class Admin
+{
+    public void adminFunc(JsonClassGerechten newgerechten)
+    {
+        JsonClassGerechten gerechtenJson = new JsonClassGerechten();
+        Console.WriteLine("Wat is uw admin gebruiksernaam?");
+        var naam = Console.ReadLine();
+        Console.WriteLine("Wat is uw admin wachtwoord?");
+        var ww = Console.ReadLine();
+        if (naam == "Admin" && ww == "Admin123")
+        {
+            Console.WriteLine("Logged In!");
+            Console.WriteLine("Van welke dag wilt u iets veranderen in het menu?");
+            var dag = Console.ReadLine();
+            if (dag == "maandag" || dag == "Maandag") {
+                gerechtenJson.maandag = new string[11][];
+                for(int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine("Naam gerecht:");
+                    string naamgerecht = Console.ReadLine();
+                    var result1 = naamgerecht == "" ? "NoName" : naamgerecht;
+                    Console.WriteLine("Beschrijving gerecht:");
+                    var beschrijvinggerecht = Console.ReadLine();
+                    var result2 = beschrijvinggerecht == "" ? "Een lekker gerecht" : beschrijvinggerecht;
+                    Console.WriteLine("Allergenen gerecht:");
+                    var allergenengerecht = Console.ReadLine();
+                    var result3 = allergenengerecht == "" ? " " : allergenengerecht;
+                    Console.WriteLine("Prijs gerecht:");
+                    var prijsgerecht = Console.ReadLine();
+                    var result4 = prijsgerecht == "" ? "10,00 Euro" : prijsgerecht;
+                    gerechtenJson.maandag[i] = new string[] { result1, result2, result3, result4 };
+                }
+                gerechtenJson.dinsdag = newgerechten.dinsdag;
+                gerechtenJson.woensdag = newgerechten.woensdag;
+                gerechtenJson.donderdag = newgerechten.donderdag;
+                gerechtenJson.vrijdag = newgerechten.vrijdag;
+                gerechtenJson.zaterdag = newgerechten.zaterdag;
+                gerechtenJson.zondag = newgerechten.zondag;
+
+                string nieuw = JsonConvert.SerializeObject(gerechtenJson);
+                File.WriteAllText(@"gerechten.json", nieuw);
+            if (dag == "dinsdag" || dag == "Dinsdag")
+            {
+                gerechtenJson.dinsdag = new string[11][];
+                for (int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine("Naam gerecht:");
+                    string naamgerecht = Console.ReadLine();
+                    var result1 = naamgerecht == "" ? "NoName" : naamgerecht;
+                    Console.WriteLine("Beschrijving gerecht:");
+                    var beschrijvinggerecht = Console.ReadLine();
+                    var result2 = beschrijvinggerecht == "" ? "Een lekker gerecht" : beschrijvinggerecht;
+                    Console.WriteLine("Allergenen gerecht:");
+                    var allergenengerecht = Console.ReadLine();
+                    var result3 = allergenengerecht == "" ? " " : allergenengerecht;
+                    Console.WriteLine("Prijs gerecht:");
+                    var prijsgerecht = Console.ReadLine();
+                    var result4 = prijsgerecht == "" ? "10,00 Euro" : prijsgerecht;
+                    gerechtenJson.dinsdag[i] = new string[] { result1, result2, result3, result4 };
+                }
+                gerechtenJson.maandag = newgerechten.maandag;
+                gerechtenJson.woensdag = newgerechten.woensdag;
+                gerechtenJson.donderdag = newgerechten.donderdag;
+                gerechtenJson.vrijdag = newgerechten.vrijdag;
+                gerechtenJson.zaterdag = newgerechten.zaterdag;
+                gerechtenJson.zondag = newgerechten.zondag;
+
+                string nieuw1 = JsonConvert.SerializeObject(gerechtenJson);
+                File.WriteAllText(@"gerechten.json", nieuw1);
+                }
+                string nieuwhier = JsonConvert.SerializeObject(gerechtenJson);
+                File.WriteAllText(@"gerechten.json", nieuwhier);
+            }
+        
+    }
+}
--- Restaurant Application/Main.cs
+++ Restaurant Application/Main.cs
@@ -12,6 +12,15 @@ namespace oefenen1
             JsonClass gebruikerJson = new JsonClass();
             Customerlogin loginClass = new Customerlogin();
             email emailClass = new email();
+<<<<<<< Updated upstream
+=======
+            JsonClassGerechtenIngevuld gerechtenIngevuldClass = new JsonClassGerechtenIngevuld();
+            JsonClassReservering reserveringJson = new JsonClassReservering();
+            Reservering ReserveringClass = new Reservering();
+            Tafel tafelClass = new Tafel();
+            JsonClassTafels tafelJson = new JsonClassTafels();
+            Admin adminClass = new Admin();
+>>>>>>> Stashed changes
 
             JsonClassGerechten gerechtenJson = new JsonClassGerechten();
             gerechtenJson.a = 5;
@@ -153,10 +162,69 @@ namespace oefenen1
             }
             else if (menu_of_reservering == "3")
             {
+<<<<<<< Updated upstream
                 JsonClassReservering reserveringJson = new JsonClassReservering();
                 Reservering ReserveringClass = new Reservering();
                 ReserveringClass.reserveringFunc(reserveringJson);
                 
+=======
+                Console.WriteLine("Welkom bij ons restaurant");
+                Console.WriteLine("U kunt via deze applicatie een reservering plaatsen en het menu bekijken");
+                Console.WriteLine("Wilt u een menu bekijken type(1), wilt u een gerecht opzoeken type(2), wilt u een reservering maken type(3), wilt u inloggen als admin type(4)");
+                var menu_of_reservering = Console.ReadLine();
+                if (menu_of_reservering == "1")
+                {
+                    Console.WriteLine("Van welke dag wilt u het menu bekijken");
+                    var day = Console.ReadLine();
+                    Menu menuVanEenDagClass = new Menu();
+                    menuVanEenDagClass.MenuVanDeDagFunc(day, gerechtenIngevuldClass.gerechtenIngevuldFunc());
+                }
+                else if (menu_of_reservering == "2")
+                {
+                    Console.WriteLine("welke gerecht wil je ?");
+                    var gerecht = Console.ReadLine();
+                    gerechten gerechtenClass = new gerechten();
+                    gerechtenClass.gerechtenFunc(gerecht, gerechtenIngevuldClass.gerechtenIngevuldFunc());
+                }
+                else if (menu_of_reservering == "3")
+                {
+                    Console.WriteLine("om een reservering te plaatsen moet u eerst inloggen of een account aanmaken");
+                    int loginFail = loginClass.LoginFunc(gebruikerJson);
+                    if (loginFail == 0)
+                    {
+                        Console.WriteLine("u kunt nu een reservering plaatsen");
+                        ReserveringClass.reserveringFunc(reserveringJson, tafelClass.tafelFunc());
+                    }
+                    if (loginFail == 1)
+                    {
+                        emailClass.emailFunc(gebruikerJson);
+                    }
+                    if (loginFail == 2)
+                    {
+                        Console.WriteLine("Wilt u een bestaande reservering bekijken type(1) of wilt u een nieuwe reservering aanmaken type(2)");
+                        var Ingelogd = Console.ReadLine();
+                        if (Ingelogd == "1") 
+                        {
+                            JsonClassReservering ingelogdModus = JsonConvert.DeserializeObject<JsonClassReservering>(File.ReadAllText(@"reservering_id.json"));
+                            using (StreamReader file = File.OpenText(@"reservering_id.json"))
+                            {
+                                JsonSerializer serializer = new JsonSerializer();
+                                JsonClassReservering leesReservering = (JsonClassReservering)serializer.Deserialize(file, typeof(JsonClassReservering));
+                                Console.WriteLine(leesReservering.Datum + "\n" + leesReservering.Tijden + "\n" + leesReservering.Personen + "\n" + leesReservering.Details);
+                            }
+                        }
+                        else if (Ingelogd == "2")
+                        {
+                            Console.WriteLine("u kunt nu een reservering plaatsen");
+                            ReserveringClass.reserveringFunc(reserveringJson, tafelClass.tafelFunc());
+                        }
+                    }
+                }
+                else if (menu_of_reservering == "4")
+                {
+                    adminClass.adminFunc(gerechtenIngevuldClass.gerechtenIngevuldFunc());
+                }
+>>>>>>> Stashed changes
             }
         }
     }
